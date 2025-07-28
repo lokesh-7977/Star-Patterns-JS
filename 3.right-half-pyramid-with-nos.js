9b7b82a0a5d0200b7fd8ca7bdcd9af8f74f754c0
Lokesh Medharametla <73935267+lokesh-7977@users.noreply.github.com>
2025-07-28 17:32:00 +0000

    Add right half pyramid pattern generation with numbers

---
+++ 3.right-half-pyramid-with-nos.js
@@ -0,0 +1,18 @@
+let n = 6 
+
+for(let i =0; i< n ; i++){
+    let row = " ";
+    for(let j=1;j<=i;j++){
+        row = row + " " +j
+    }
+    console.log(row)
+}
+
+//Output will be:
+
+//  1
+//  1 2
+//  1 2 3
+//  1 2 3 4
+//  1 2 3 4 5
+//  1 2 3 4 5 6
