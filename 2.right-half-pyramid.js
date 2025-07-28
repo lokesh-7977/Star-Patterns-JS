af90f3c8030cb97c40da287c023877ec7f79dd2f
Lokesh Medharametla <73935267+lokesh-7977@users.noreply.github.com>
2025-07-28 17:18:16 +0000

    Implement right half pyramid pattern generation

---
+++ 2.right-half-pyramid.js
@@ -0,0 +1,18 @@
+let n = 6 
+
+for(let i =0; i< n ; i++){
+    let row = " ";
+    for(let j=0;j<i;j++){
+        row = row + " *";
+    }
+    console.log(row)
+}
+
+//Output will be:
+
+//  *
+//  * *
+//  * * *
+//  * * * *
+//  * * * * *
+//  * * * * * *
