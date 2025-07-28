99d37829fb334a46909de2b22d816f4bf49d6b07
Lokesh Medharametla <73935267+lokesh-7977@users.noreply.github.com>
2025-07-28 17:08:55 +0000

    Add rectangle pattern generation

---
+++ 1.rectangle.js
@@ -0,0 +1,19 @@
+ let n =6 // n might be anytghing
+ 
+ for(let i=0;i<n;i++){
+    let row="";
+    for(let j=0;j<n;j++){
+        row = row + " *"
+    }
+    console.log(row)
+}
+
+//Output will be:
+
+//  * * * * * *
+//  * * * * * *
+//  * * * * * *
+//  * * * * * *
+//  * * * * * *
+//  * * * * * *
+
